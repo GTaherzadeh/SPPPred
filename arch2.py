20c6c5b4ff65e12cdfe8ede6f92288aac7b615a4
G <64107627+GTaherzadeh@users.noreply.github.com>
2023-12-04 10:57:16 -0500

    arch2.py

--- arch2.py
+++ arch2.py
@@ -1,85 +1,87 @@
+#read file
 import pandas as pd
 import os
 
-test_path = os.path.join("data", "Test.Set.xlsx")
-from pandas import read_excel
-from utils import features
-import time
-my_sheet = 'Sheet1' 
-orig_df = read_excel(test_path, sheet_name = my_sheet)
+file_names = ["out_peptide","out_RNA", "out_DNA", "out_CBH"]
 
-df = pd.DataFrame(orig_df, columns = features)
+for file_name in file_names:
 
-from utils import indexing
+    test_path = os.path.join(f"{file_name}.xlsx")
+    from pandas import read_excel
+    # from utils import features
+    import time
+    my_sheet = 'Sheet1' 
+    orig_df = read_excel(test_path, sheet_name = my_sheet)
+    # print(orig_df.head())
+    df = pd.DataFrame(orig_df)
 
-HSE_values = indexing(df['HSE'])
-SS_values = indexing(df['SS'])
-ASA_values = indexing(df['ASA'])
-PSSM_values = indexing(df['PSSM'])
-SEQ_values = indexing(df['SEQ'])
-Physicochemical properties _values = indexing(df['Physicochemical properties'])
-df['HSE'] = df['HSE'].apply(lambda x: HSE_values[x])
-df['SS'] = df['SS'].apply(lambda x: SS_values[x])
-df['ASA'] = df['ASA'].apply(lambda x: ASA_values[x])
-df['PSSM'] = df['PSSM'].apply(lambda x: PSSM_values[x])
-df['SEQ'] = df['SEQ'].apply(lambda x: SEQ_values[x])
-df['Physicochemical properties'] = df['Physicochemical properties'].apply(lambda x:  Physicochemical properties_values[x])
+    from utils import indexing
 
-from utils import remove_nan_features
-filtered_features = remove_nan_features(df, features)
+    SS_values = indexing(df['SS'])
+    AA_values = indexing(df['AA'])
+    df['SS'] = df['SS'].apply(lambda x: SS_values[x])
+    df['AA'] = df['AA'].apply(lambda x: AA_values[x])
+    # The complete features file is available separately
 
-df = pd.DataFrame(df, columns = filtered_features)
-HSE_values = indexing(df['HSE'])
-SS_values = indexing(df['SS'])
-ASA_values = indexing(df['ASA'])
-PSSM_values = indexing(df['PSSM'])
-SEQ_values = indexing(df['SEQ'])
-Physicochemical properties_values = indexing(df['Physicochemical properties'])
-df['HSE'] = df['HSE'].apply(lambda x: HSE_values[x])
-df['SS'] = df['SS'].apply(lambda x: SS_values[x])
-df['ASA'] = df['ASA'].apply(lambda x: ASA_values[x])
-df['PSSM'] = df['PSSM'].apply(lambda x: PSSM_values[x])
-df['SEQ'] = df['SEQ'].apply(lambda x: SEQ_values[x])
-df['Physicochemical properties'] = df['Physicochemical properties'].apply(lambda x:Physicochemical properties_values[x])
+    # remove Nan features 
+    from utils import remove_nan_features
+    filtered_features = remove_nan_features(df, df.columns)
 
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
+    df = pd.DataFrame(df, columns = filtered_features)
+    SS_values = indexing(df['SS'])
+    AA_values = indexing(df['AA'])
+    df['SS'] = df['SS'].apply(lambda x: SS_values[x])
+    df['AA'] = df['AA'].apply(lambda x: AA_values[x])
 
 
-import numpy as np
-import pandas as pd
-
-start_time = time.time()
-
-import numpy as np
-from numpy import array
-
-selected_features += ['Label']
-print('selected_features', selected_features)
-
-print(len(features), len(selected_features))
+    import warnings
+    warnings.filterwarnings("ignore", category=FutureWarning)
 
 
-textfile = open("selected_features_arch2.txt", "w")
+    import numpy as np
+    import pandas as pd
 
-for element in selected_features:
+    start_time = time.time()
 
-    textfile.write(element + "\n")
+    import numpy as np
+    from s_a import simulated_annealing, objective
 
-textfile.close()
+    selected_features = []
+    for i in filtered_features:
+        bounds = np.asarray([[df[i].min(), df[i].max()]])
+        n_iterations = 100
+        step_size = 0.1
+        df['temp'] = df[i].apply(
+        lambda x: simulated_annealing(objective, bounds, n_iterations, step_size, x)[0])
+        try:
+            if int(df['temp'].mean()[0]/ df[i].mean()) > 2 or int(df[i].mean()/df['temp'].mean()[0]):
+                selected_features.append(i)
+        except:
+            pass
 
-print(len(features), len(selected_features))
+    selected_features += ['Label']
+    print('selected_features', selected_features)
 
-df = pd.DataFrame(df, columns = selected_features)
-
-
-spand_time = time.time() - start_time
-print("spand_time is: ", spand_time)
-
-with open("arch2/spand_time_select_features.txt" ,'w') as f:
-    f.write(str(spand_time))
-
-df.to_csv('arch2/select_features_data.csv')
+    # print(len(features), len(selected_features))
 
 
+    textfile = open(f"arch2/selected_features_arch2_{file_name}.txt", "w")
 
+    for element in selected_features:
+
+        textfile.write(element + "\n")
+
+    textfile.close()
+
+    # print(len(features), len(selected_features))
+
+    df = pd.DataFrame(df, columns = selected_features)
+
+
+    spand_time = time.time() - start_time
+    print("spand_time is: ", spand_time)
+
+    with open(f"arch2/spand_time_select_features_{file_name}.txt" ,'w') as f:
+        f.write(str(spand_time))
+
+    df.to_csv(f'arch2/select_features_data_{file_name}.csv') 
